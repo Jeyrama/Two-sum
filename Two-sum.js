ec22e8d83d8d25196c85e73d1f450c3dca9be12e
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-17 17:15:45 -0700

    Update Two-sum.js

--- Two-sum.js
+++ Two-sum.js
@@ -24,3 +24,11 @@ function twoSum(numbers, target) {
 }
 
 // or
+
+function twoSum(numbers, target) {
+  for (let i = 0; i < numbers.length-1; i++) {
+    for (let j = i+1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) return [i, j];
+    }
+  }
+}
